74fe641bc9860dbd704dd750566c3ae2bbbcf962
omiinaya <omiinaya@gmail.com>
2020-03-28 13:55:51 -0400

    y gte and lte no work men :(

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -6,54 +6,70 @@
 // =============================================================
 var Books = require("../models/book.js");
 
-
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // Add sequelize code to get all books and return them as JSON
-  app.get("/api/all", function(req, res) {
-    Books.findAll({})
-        .then(function(result) {
-          return res.json(result);
-        });
+  app.get("/api/all", function (req, res) {
+    Books.findAll({}).then(function (result) {
+      res.json(result);
+    });
   });
 
   // Add sequelize code to get a specific book and return it as JSON
-  app.get("/api/:book", function(req, res) {
+  app.get("/api/:book", function (req, res) {
     if (req.params.books) {
-      Books.findOne({
+      Books.findAll({
         where: {
           title: req.params.books
         }
-      }).then(function(result) {
-        return res.json(result);
+      }).then(function (result) {
+        res.json(result);
       });
     }
   });
 
   // Add sequelize code to get all books of a specific genre and return them as JSON
-  app.get("/api/genre/:genre", function(req, res) {
-    
+  app.get("/api/genre/:genre", function (req, res) {
+
   });
 
   // Add sequelize code to get all books from a specific author and return them as JSON
-  app.get("/api/author/:author", function(req, res) {
+  app.get("/api/author/:author", function (req, res) {
 
   });
 
   // Add sequelize code to get all "long" books (more than 150 pages) and return them as JSON
-  app.get("/api/books/long", function(req, res) {
-
+  app.get("/api/books/long", function (req, res) {
+    Books.findAll({
+      WHERE: {
+        PAGES: {
+          $gte: 150
+        }
+      },
+      order: [["pages", "DESC"]]
+    }).then(function (results) {
+      res.json(results)
+    });
   });
 
   // Add sequelize code to get all "short" books (150 pages or less) and return them as JSON
-  app.get("/api/books/short", function(req, res) {
-
+  app.get("/api/books/short", function (req, res) {
+    Books.findAll({
+      WHERE: {
+        PAGES: {
+          $lte: 150
+        }
+      },
+      order: [["pages", "DESC"]]
+    }).then(function (results) {
+      res.json(results)
+    });
   });
 
   // Add sequelize code to create a book
-  app.post("/api/new", function(req, res) {
+  app.post("/api/new", function (req, res) {
     var book = req.body;
 
     // Then add the character to the database using sequelize
@@ -68,7 +84,7 @@ module.exports = function(app) {
 
 
   // Add sequelize code to delete a book
-  app.delete("/api/book/:id", function(req, res) {
+  app.delete("/api/book/:id", function (req, res) {
 
   });
 
