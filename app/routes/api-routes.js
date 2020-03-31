b27c234862048d96399c8a1a014a96706fb68c5f
Omiinaya <omiinaya@gmail.com>
2020-03-31 15:21:38 -0400

    Update api-routes.js

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -4,88 +4,107 @@
 
 // Dependencies
 // =============================================================
-var Books = require("../models/book.js");
+var Book = require("../models/book.js");
+
 
 // Routes
 // =============================================================
-module.exports = function (app) {
+module.exports = function(app) {
 
   // Add sequelize code to get all books and return them as JSON
-  app.get("/api/all", function (req, res) {
-    Books.findAll({}).then(function (result) {
-      res.json(result);
+  app.get("/api/all", function(req, res) {
+    Book.findAll({}).then(function(results){
+      res.json(results);
     });
   });
 
   // Add sequelize code to get a specific book and return it as JSON
-  app.get("/api/:book", function (req, res) {
-    if (req.params.books) {
-      Books.findAll({
+  app.get("/api/:book", function(req, res) {
+    if(req.params.book){
+      Book.findAll({
         where: {
-          title: req.params.books
+          title: req.params.book
         }
-      }).then(function (result) {
-        res.json(result);
+      }).then(function(results){
+        res.json(results);
       });
     }
   });
 
   // Add sequelize code to get all books of a specific genre and return them as JSON
-  app.get("/api/genre/:genre", function (req, res) {
-
+  app.get("/api/genre/:genre", function(req, res) {
+    if(req.params.genre){
+      Book.findAll({
+        where: {
+          genre: req.params.genre
+        }
+      }).then(function(results){
+        res.json(results);
+      });
+    }
   });
 
   // Add sequelize code to get all books from a specific author and return them as JSON
-  app.get("/api/author/:author", function (req, res) {
-
+  app.get("/api/author/:author", function(req, res) {
+    if(req.params.author){
+      Book.findAll({ 
+      where:{
+        author: req.params.author
+      }
+    }).then(function(results){
+      res.json(results);
+    });
+   }
   });
 
   // Add sequelize code to get all "long" books (more than 150 pages) and return them as JSON
-  app.get("/api/books/long", function (req, res) {
-    Books.findAll({
-      WHERE: {
-        PAGES: {
-          $gte: 150
+  app.get("/api/books/long", function(req, res) {
+    Book.findAll({
+      where: {
+        pages: {
+          $gte: 300
         }
       },
       order: [["pages", "DESC"]]
-    }).then(function (results) {
-      res.json(results)
+    }).then(function(results){
+      res.json(results);
     });
   });
 
   // Add sequelize code to get all "short" books (150 pages or less) and return them as JSON
-  app.get("/api/books/short", function (req, res) {
-    Books.findAll({
-      WHERE: {
-        PAGES: {
+  app.get("/api/books/short", function(req, res) {
+    Book.findAll({
+      where: {
+        pages: {
           $lte: 150
         }
       },
-      order: [["pages", "DESC"]]
-    }).then(function (results) {
-      res.json(results)
+      order: [["pages", "ASC"]]
+    }).then(function(results){
+      res.json(results);
     });
   });
 
   // Add sequelize code to create a book
-  app.post("/api/new", function (req, res) {
-    var book = req.body;
-
-    // Then add the character to the database using sequelize
-    Books.create({
-      title: book.title,
-      author: book.author,
-      genre: book.genre,
-      pages: book.pages
+  app.post("/api/new", function(req, res) {
+    console.log("Book Data:");
+    console.log(req.body);
+    Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      pages:req.body.pages
     });
   });
 
-
-
   // Add sequelize code to delete a book
-  app.delete("/api/book/:id", function (req, res) {
-
+  app.post("/api/delete", function(req, res) {
+    console.log("Book Data:");
+    console.log(req.body);
+    Book.destroy({
+      where: {
+        id: req.body.id
+      }
+    });
   });
-
 };
