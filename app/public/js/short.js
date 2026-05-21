abbd7f3f8985fbf5d3b6ac0b1b3e9affb5699613
omiinaya <omiinaya@gmail.com>
2026-05-21 07:29:43 -0400

    fix: replace var with const to address code smell violations

--- app/public/js/short.js
+++ app/public/js/short.js
@@ -1,22 +1,23 @@
-// Make a get request to our api route that will return every "short" book (150 pages or less)
-$.get('/api/books/short', function (data) {
-  // For each book that our server sends us back
-  for (let i = 0; i < data.length; i++) {
-    // Create a parent div to hold book data
-    const wellSection = $('<div>');
-    // Add a class to this div: 'well'
-    wellSection.addClass('well');
-    // Add an id to the well to mark which well it is
-    wellSection.attr('id', 'book-well-' + i);
-    // Append the well to the well section
-    $('#well-section').append(wellSection);
-
-    // Now  we add our book data to the well we just placed on the page
-    $('#book-well-' + i).append(
-      '<h2>' + (i + 1) + '. ' + data[i].title + '</h2>',
-    );
-    $('#book-well-' + i).append('<h3>Author: ' + data[i].author + '</h4>');
-    $('#book-well-' + i).append('<h3>Genre: ' + data[i].genre + '</h4>');
-    $('#book-well-' + i).append('<h3>Pages: ' + data[i].pages + '</h4>');
-  }
-});
+     1|// Make a get request to our api route that will return every "short" book (150 pages or less)
+     2|$.get('/api/books/short', function (data) {
+     3|  // For each book that our server sends us back
+     4|  for (let i = 0; i < data.length; i++) {
+     5|    // Create a parent div to hold book data
+     6|    const wellSection = $('<div>');
+     7|    // Add a class to this div: 'well'
+     8|    wellSection.addClass('well');
+     9|    // Add an id to the well to mark which well it is
+    10|    wellSection.attr('id', 'book-well-' + i);
+    11|    // Append the well to the well section
+    12|    $('#well-section').append(wellSection);
+    13|
+    14|    // Now  we add our book data to the well we just placed on the page
+    15|    $('#book-well-' + i).append(
+    16|      '<h2>' + (i + 1) + '. ' + data[i].title + '</h2>',
+    17|    );
+    18|    $('#book-well-' + i).append('<h3>Author: ' + data[i].author + '</h4>');
+    19|    $('#book-well-' + i).append('<h3>Genre: ' + data[i].genre + '</h4>');
+    20|    $('#book-well-' + i).append('<h3>Pages: ' + data[i].pages + '</h4>');
+    21|  }
+    22|});
+    23|
--- app/public/js/view.js
+++ app/public/js/view.js
@@ -1,77 +1,78 @@
-// When user hits the search-btn
-$('#search-btn').on('click', function (event) {
-  event.preventDefault();
-
-  // Save the book they typed into the book-search input
-  const bookSearched = $('#book-search').val().trim();
-
-  // Make an AJAX get request to our api, including the user's book in the url
-  $.get('/api/' + bookSearched, function (data) {
-    console.log(data);
-    // Call our renderBooks function to add our books to the page
-    renderBooks(data);
-  });
-});
-
-// When user hits the author-search-btn
-$('#author-search-btn').on('click', function () {
-  // Save the author they typed into the author-search input
-  const authorSearched = $('#author-search').val().trim();
-
-  // Make an AJAX get request to our api, including the user's author in the url
-  $.get('/api/author/' + authorSearched, function (data) {
-    // Log the data to the console
-    console.log(data);
-    // Call our renderBooks function to add our books to the page
-    renderBooks(data);
-  });
-});
-
-// When user hits the genre-search-btn
-$('#genre-search-btn').on('click', function () {
-  // Save the book they typed into the genre-search input
-  const genreSearched = $('#genre-search').val().trim();
-
-  // Make an AJAX get request to our api, including the user's genre in the url
-  $.get('/api/genre/' + genreSearched, function (data) {
-    console.log(data);
-    // Call our renderBooks function to add our books to the page
-    renderBooks(data);
-  });
-});
-
-function renderBooks(data) {
-  if (data.length !== 0) {
-    $('#stats').empty();
-    $('#stats').show();
-
-    for (let i = 0; i < data.length; i++) {
-      const div = $('<div>');
-
-      div.append('<h2>' + data[i].title + '</h2>');
-      div.append('<p>Author: ' + data[i].author + '</p>');
-      div.append('<p>Genre: ' + data[i].genre + '</p>');
-      div.append('<p>Pages: ' + data[i].pages + '</p>');
-      div.append(
-        '<button class=\'delete\' data-id=\'' +
-          data[i].id +
-          '\'>DELETE BOOK</button>',
-      );
-
-      $('#stats').append(div);
-    }
-
-    $('.delete').click(function () {
-      $.ajax({
-        method: 'DELETE',
-        url: '/api/book/' + $(this).attr('data-id'),
-      })
-        // On success, run the following code
-        .then(function () {
-          console.log('Deleted Successfully!');
-        });
-
-      $(this).closest('div').remove();
-    });
-  }
-}
+     1|// When user hits the search-btn
+     2|$('#search-btn').on('click', function (event) {
+     3|  event.preventDefault();
+     4|
+     5|  // Save the book they typed into the book-search input
+     6|  const bookSearched = $('#book-search').val().trim();
+     7|
+     8|  // Make an AJAX get request to our api, including the user's book in the url
+     9|  $.get('/api/' + bookSearched, function (data) {
+    10|    console.log(data);
+    11|    // Call our renderBooks function to add our books to the page
+    12|    renderBooks(data);
+    13|  });
+    14|});
+    15|
+    16|// When user hits the author-search-btn
+    17|$('#author-search-btn').on('click', function () {
+    18|  // Save the author they typed into the author-search input
+    19|  const authorSearched = $('#author-search').val().trim();
+    20|
+    21|  // Make an AJAX get request to our api, including the user's author in the url
+    22|  $.get('/api/author/' + authorSearched, function (data) {
+    23|    // Log the data to the console
+    24|    console.log(data);
+    25|    // Call our renderBooks function to add our books to the page
+    26|    renderBooks(data);
+    27|  });
+    28|});
+    29|
+    30|// When user hits the genre-search-btn
+    31|$('#genre-search-btn').on('click', function () {
+    32|  // Save the book they typed into the genre-search input
+    33|  const genreSearched = $('#genre-search').val().trim();
+    34|
+    35|  // Make an AJAX get request to our api, including the user's genre in the url
+    36|  $.get('/api/genre/' + genreSearched, function (data) {
+    37|    console.log(data);
+    38|    // Call our renderBooks function to add our books to the page
+    39|    renderBooks(data);
+    40|  });
+    41|});
+    42|
+    43|function renderBooks(data) {
+    44|  if (data.length !== 0) {
+    45|    $('#stats').empty();
+    46|    $('#stats').show();
+    47|
+    48|    for (let i = 0; i < data.length; i++) {
+    49|      const div = $('<div>');
+    50|
+    51|      div.append('<h2>' + data[i].title + '</h2>');
+    52|      div.append('<p>Author: ' + data[i].author + '</p>');
+    53|      div.append('<p>Genre: ' + data[i].genre + '</p>');
+    54|      div.append('<p>Pages: ' + data[i].pages + '</p>');
+    55|      div.append(
+    56|        '<button class=\'delete\' data-id=\'' +
+    57|          data[i].id +
+    58|          '\'>DELETE BOOK</button>',
+    59|      );
+    60|
+    61|      $('#stats').append(div);
+    62|    }
+    63|
+    64|    $('.delete').click(function () {
+    65|      $.ajax({
+    66|        method: 'DELETE',
+    67|        url: '/api/book/' + $(this).attr('data-id'),
+    68|      })
+    69|        // On success, run the following code
+    70|        .then(function () {
+    71|          console.log('Deleted Successfully!');
+    72|        });
+    73|
+    74|      $(this).closest('div').remove();
+    75|    });
+    76|  }
+    77|}
+    78|
